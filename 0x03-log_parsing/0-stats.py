0a8de69c07dae483f7e18c9076596ea495289cc8
Chalo1996 <emusyoka759@gmail.com>
2023-02-17 02:18:14 +0300

    reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
 """Script that reads stdin line by line and computes metrics."""
 
+
 import sys
 import signal
 from typing import Dict
 
+total_size: int = 0
+status_counts: Dict[int, int] = {}
 
-def print_stats(_: int, __: int) -> None:
-    """Signal handler function to print statistics.
 
-    Prints the total file size and the number of lines with each
-    status code, sorted in ascending order.
-
-    Args:
-        signum (int): The signal number (ignored).
-        frame: The current stack frame (ignored).
-    """
-    global total_size, status_counts
-
+def print_stats() -> None:
+    """Prints statistics for the current state of the program."""
     print("Total file size:", total_size)
     for status_code in sorted(status_counts.keys()):
         print(f"{status_code}: {status_counts[status_code]}")
 
 
-signal.signal(signal.SIGINT, print_stats)
-
-total_size: int = 0
-status_counts: Dict[int, int] = {}
+signal.signal(signal.SIGINT, lambda signum, frame: print_stats())
 
 for i, line in enumerate(sys.stdin):
     line = line.strip()
@@ -42,6 +33,6 @@ for i, line in enumerate(sys.stdin):
     status_counts[status_code] = status_counts.get(status_code, 0) + 1
 
     if (i + 1) % 10 == 0:
-        print_stats(None, None)
+        print_stats()
 
-print_stats(None, None)
+print_stats()
