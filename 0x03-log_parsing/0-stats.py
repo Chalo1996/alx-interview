4d6e635727cdad8a6c84459726a8a00e058e1d27
Chalo1996 <emusyoka759@gmail.com>
2023-02-17 02:29:15 +0300

    reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 """Script that reads stdin line by line and computes metrics."""
 
-
 import sys
 import signal
 from typing import Dict
 
-total_size: int = 0
-status_counts: Dict[int, int] = {}
+total_size = 0
+status_counts = {}
 
 
-def print_stats() -> None:
+def print_stats(signum, frame):
     """Prints statistics for the current state of the program."""
-    print("Total file size:", total_size)
+    print("Total file size: {}".format(total_size))
     for status_code in sorted(status_counts.keys()):
-        print(f"{status_code}: {status_counts[status_code]}")
+        print("{}: {}".format(status_code, status_counts[status_code]))
 
 
-signal.signal(signal.SIGINT, lambda signum, frame: print_stats())
+signal.signal(signal.SIGINT, print_stats)
 
 for i, line in enumerate(sys.stdin):
     line = line.strip()
@@ -26,13 +25,13 @@ for i, line in enumerate(sys.stdin):
         parts = line.split()
         file_size = int(parts[-1])
         status_code = int(parts[-2])
-    except BaseException:
+    except:
         continue
 
     total_size += file_size
     status_counts[status_code] = status_counts.get(status_code, 0) + 1
 
     if (i + 1) % 10 == 0:
-        print_stats()
+        print_stats(None, None)
 
-print_stats()
+print_stats(None, None)
