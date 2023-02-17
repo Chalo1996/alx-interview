cee342f5f8160097f821ec2063792b123a03033e
Chalo1996 <emusyoka759@gmail.com>
2023-02-17 03:13:50 +0300

    reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
 """Script that reads stdin line by line and computes metrics."""
 
+
 import sys
-import signal
-from typing import Dict
+import re
 
-total_size = 0
-status_counts = {}
+total_size = 0  # total size of all files
+num_lines_by_status_code = {}
 
+# Read stdin line by line
+for line in sys.stdin:
+    line = sys.stdin.readline()
+    match = re.search(r'^ (\S + ) - \[(.*?)\] "(.*?)" (\d+) (\S + )', line)
+    if match:
+        status_code = match.group(4)
+        file_size = match.group(5)
+        if not status_code.isdigit():
+            continue
 
-def print_stats(signum, frame):
-    """Prints statistics for the current state of the program."""
-    print("File size: {}".format(total_size))
-    for status_code in sorted(status_counts.keys()):
-        print("{}: {}".format(status_code, status_counts[status_code]))
+        # Sum up file sizes for each valid line
+        file_size = int(file_size)
+        total_size += file_size
 
+        # Increment number of lines by status code
+        if status_code not in num_lines_by_status_code:
+            num_lines_by_status_code[status_code] = 0
+        num_lines_by_status_code[status_code] += 1
 
-signal.signal(signal.SIGINT, print_stats)
-
-for i, line in enumerate(sys.stdin):
-    line = line.strip()
-
-    try:
-        parts = line.split()
-        file_size = int(parts[-1])
-        status_code = int(parts[-2])
-    except BaseException:
-        continue
-
-    total_size += file_size
-    status_counts[status_code] = status_counts.get(status_code, 0) + 1
-
-    if (i + 1) % 10 == 0:
-        print_stats(None, None)
-
-print_stats(None, None)
+        # Print the output after every 10 lines or a keyboard interruption
+        if (sys.stdin.readline() % 10 == 0) and sys.stdin.isatty():
+            print("Total file size: {}".format(total_size))
+            for k, v in sorted(num_lines_by_status_code.items()):
+                print("{}: {}".format(k, v))
