848b24fea526b1cd4c1afdc15dda4f2f1611e777
Chalo1996 <emusyoka759@gmail.com>
2023-02-18 21:05:37 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,37 +5,50 @@
 import sys
 
 
-status_dict = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0}
+def log_parse():
+    """Function that reads stdin line by line and computes metrics.
 
-total_size = 0
+    Args:
+        None
 
-while True:
-    for line in sys.stdin:
-        line = line.strip()
-        line = line.split()
+    Returns:
+        None
+    """
+    status_dict = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0}
 
-        if len(line) > 2:
-            try:
-                status = line[-2]
-                size = line[-1]
+    total_size = 0
 
-                for k, v in status_dict.items():
-                    if status == k:
-                        status_dict[k] += 1
-                        total_size += int(size)
-                print('File size: {}'.format(total_size))
-                for key in sorted(status_dict):
-                    print('{}: {}'.format(key, status_dict[key]))
+    while True:
+        for line in sys.stdin:
+            line = line.strip()
+            line = line.split()
 
-            except KeyboardInterrupt:
-                print('File size: {}'.format(total_size))
-                for key in sorted(status_dict):
-                    print('{}: {}'.format(key, status_dict[key]))
+            if len(line) > 2:
+                try:
+                    status = line[-2]
+                    size = line[-1]
+
+                    for k, v in status_dict.items():
+                        if status == k:
+                            status_dict[k] += 1
+                            total_size += int(size)
+                    print('File size: {}'.format(total_size))
+                    for key in sorted(status_dict):
+                        print('{}: {}'.format(key, status_dict[key]))
+
+                except KeyboardInterrupt:
+                    print('File size: {}'.format(total_size))
+                    for key in sorted(status_dict):
+                        print('{}: {}'.format(key, status_dict[key]))
+
+
+if __name__ == '__main__':
+    log_parse()
