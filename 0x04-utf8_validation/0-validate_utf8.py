63167fccb06eadea081d95047702b018c132c3f7
Chalo1996 <emusyoka759@gmail.com>
2023-02-23 16:12:12 +0300

    UTF-8 validator

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,16 +5,19 @@
 from typing import List
 
 
-def validUTF8(data: List[int]) -> bool:
+def validUTF8(data):
+    # type: (List[int]) -> bool
     """
     validUTF8: determines if a given data set represents a valid \
         UTF-8 encoding.
 
     Args:
         data: list of integers.
-    """
 
-    num_bytes: int = 0
+    Returns:
+        True if data is a valid UTF-8 encoding, else return False.
+    """
+    num_bytes = 0
     for num in data:
         if num_bytes == 0:
             if (num >> 5) == 0b110:
