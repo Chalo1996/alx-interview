9ddabd2711dff7aabd30caddbde389ba58584919
Chalo1996 <emusyoka759@gmail.com>
2023-03-13 11:44:08 +0300

    StarWars Api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,17 +4,17 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, async (err, res, body) => {
+request(url, (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
-    const charactersUrl = await JSON.parse(body).characters;
+    const charactersUrl = JSON.parse(body).characters;
     charactersUrl.forEach((characterUrl) => {
-      request(characterUrl, (err, res, body) => {
+      request(characterUrl, async (err, res, body) => {
         if (err) {
           console.log(err);
         } else {
-          console.log(JSON.parse(body).name);
+          console.log(await JSON.parse(body).name);
         }
       });
     });
